f685995042e8363667b1e35e89f447ecfcb9fe6f
Bektur Dzheentaev <dzheentaev_b@auca.kg>
2025-09-04 08:33:26 +0600

    finished task 3

---
+++ lab02/count_words.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Incorrect number of arguments provided. Received $1 instead of 2."
+    exit 1
+fi
+
+FILENAME=$1
+WORD=$2
+
+if [ ! -f "$FILENAME" ]; then
+    echo "File '$FILENAME' is not found. Please check that requested file exist."
+    exit 1
+fi
+
+COUNT=$(grep -o -i -w "$WORD" "$FILENAME" | wc -l)
+
+echo "The word '$WORD' appears $COUNT times in '$FILENAME'."
